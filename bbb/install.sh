90164b2c927643d23c465393a83eb12f463791f7
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-03-27 19:34:53 +0200

    Reduced packages

--- bbb/install.sh
+++ bbb/install.sh
@@ -12,9 +12,6 @@ apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
 
-# Add unstable branch
-# echo "deb http://ftp.us.debian.org/debian unstable main contrib non-free" > /etc/apt/sources.list.d/unstable.list
-# echo "Package: * Pin: release a=testing Pin-Priority: 100" > /etc/apt/preferences.d/unstable
 
 software=" \
 bash-completion \
@@ -28,20 +25,23 @@ screen \
 vim \
 wget \
 python-pip \
-npm \
-docker-compose \
 libusb-dev \
 nmap \
+iptables-persistent \
 libncurses5-dev
 "
-# gcc-8 \
-# g++-8 \
+# npm \
+# docker-compose \
 # apt-get update
 # apt-get upgrade
 # apt-get dist-upgrade -y
 
 apt-get install -y ${software}
-
+# Add unstable branch
+# echo "deb http://ftp.us.debian.org/debian unstable main contrib non-free" > /etc/apt/sources.list.d/unstable.list
+# echo "Package: * Pin: release a=testing Pin-Priority: 100" > /etc/apt/preferences.d/unstable
+# apt-get update
+# apt-get install gcc-8 g++-8 
 apt-get autoremove
 apt-get autoclean
 
@@ -76,19 +76,17 @@ systemctl stop docker.service
 sed -i -e 's/\/usr\/bin\/dockerd -H fd:\/\//\/usr\/bin\/dockerd -g \/mnt\/sdcard\/docker\/ -H fd:\/\//g' /lib/systemd/system/docker.service
 systemctl daemon-reload
 systemctl start docker.service
+pip install docker-compose
 
 # Update scripts
 cd /opt/scripts/boot/
 git pull
 cd 
 sed -i -e 's/\/sbin\/ifconfig usb1 192.168.6.2 netmask 255.255.255.252 || true/#\/sbin\/ifconfig usb1 192.168.6.2 netmask 255.255.255.252 || true/g' /opt/scripts/boot/autoconfigure_usb1.sh
-echo 'dhclient -1 -nw usb1' >> /opt/scripts/boot/autoconfigure_usb1.sh
+echo 'dhclient usb1' >> /opt/scripts/boot/autoconfigure_usb1.sh
 
-sed -i -e 's/#timeout 60;/timeout 30;/g' /etc/dhcp/dhclient.conf 
-sed -i -e 's/#retry 60;/retry 30;/g' /etc/dhcp/dhclient.conf 
-sed -i -e 's/#reboot 10;/reboot 10;/g' /etc/dhcp/dhclient.conf 
-sed -i -e 's/#select-timeout 5;/select-timeout 5;/g' /etc/dhcp/dhclient.conf 
-sed -i -e 's/#initial-interval 2;/initial-interval 2;/g' /etc/dhcp/dhclient.conf 
+sed -i -e 's/#timeout 60;/timeout 300;/g' /etc/dhcp/dhclient.conf 
+sed -i -e 's/#retry 60;/retry 10;/g' /etc/dhcp/dhclient.conf 
 
 # Networking
 echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
@@ -96,8 +94,7 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o usb1 -j MASQUERADE
 iptables -A FORWARD -i usb1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i wlan0 -o usb1 -j ACCEPT
-iptables-save > /etc/iptables.save
-echo 'iptables-restore < /etc/iptables.save' >> /opt/scripts/boot/autoconfigure_usb1.sh
+iptables-save > /etc/iptables/rules.v4
 echo 'ip route add 225.0.0.0/24 dev usb0' >> /opt/scripts/boot/autoconfigure_usb0.sh
 
 #
--- rpi3/install.sh
+++ rpi3/install.sh
@@ -20,7 +20,6 @@ wget \
 gcc-6 \
 g++-6 \
 python-pip \
-npm \
 docker-compose \
 libusb-dev \
 isc-dhcp-server \
@@ -28,6 +27,7 @@ iptables-persistent \
 nmap \
 libncurses5-dev
 "
+# npm \
 
 
 # apt-get update
@@ -49,7 +49,9 @@ echo -e 'network={\n    ssid="TME290_2"\n    psk="beaglebone"\n}' >> /etc/wpa_su
 wpa_cli -i wlan0 reconfigure
 
 #dhcp
-echo -e 'subnet 10.42.42.0 netmask 255.255.255.0 {\n    range 10.42.42.10 10.42.42.50;\n    option broadcast-address 10.42.42.255;\n    option routers 10.42.42.1;\n    default-lease-time 600;\n    max-lease-time 7200;\n    option domain-name "local";\n    option domain-name-servers 8.8.8.8, 8.8.4.4;\n}' >> /etc/dhcp/dhcpd.conf
+echo -e 'authoritative;\nsubnet 10.42.42.0 netmask 255.255.255.0 {\n    range 10.42.42.10 10.42.42.50;\n    option broadcast-address 10.42.42.255;\n    option routers 10.42.42.1;\n    default-lease-time 600;\n    max-lease-time 7200;\n    option domain-name "local";\n    option domain-name-servers 8.8.8.8, 8.8.4.4;\n}' >> /etc/dhcp/dhcpd.conf
+sed -i  -e 's/option domain-name "example.org";/#option domain-name "example.org";/g' /etc/dhcp/dhcpd.conf
+sed -i  -e 's/option domain-name-servers ns1.example.org, ns2.example.org;/#option domain-name-servers ns1.example.org, ns2.example.org;/g' /etc/dhcp/dhcpd.conf
 echo -e 'interface eth1\nstatic ip_address=10.42.42.1/24' >> /etc/dhcpcd.conf
 echo -e 'auto lo\niface lo inet loopback\nallow-hotplug eth1\nup iptables-restore < /etc/iptables.save' >> /etc/network/interfaces
 sed -i -e 's/INTERFACESv4=""/INTERFACESv4="eth1"/g' /etc/default/isc-dhcp-server
@@ -67,6 +69,9 @@ iptables-save > /etc/iptables/rules.v4
 
 # sed -i -e '/exit 0/isleep 30\nip route add 225.0.0.0\/24 dev eth2\nmodprobe bcm2835-v4l2\n' /etc/rc.local
 sed -i '$isleep 30\nip route add 225.0.0.0\/24 dev eth2\nmodprobe bcm2835-v4l2' /etc/rc.local
+systemctl daemon-reload
+systemctl restart dhcpcd
+systemctl restart isc-dhcp-server
 
 # if the interface are swapped
 # sed -i -e 's/eth1/eth2/g' /etc/dhcpcd.conf
