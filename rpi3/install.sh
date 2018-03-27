0c721f05ff54fc20a4c828a62c5a5eea3d40d263
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-03-27 10:32:32 +0200

    Fixed some more scripts

--- rpi3/install.sh
+++ rpi3/install.sh
@@ -62,4 +62,11 @@ iptables-save > /etc/iptables/rules.v4
 # mutlicast
 
 # sed -i -e '/exit 0/isleep 30\nip route add 225.0.0.0\/24 dev eth2\nmodprobe bcm2835-v4l2\n' /etc/rc.local
-sed '$isleep 30\nip route add 225.0.0.0\/24 dev eth2\nmodprobe bcm2835-v4l2' /etc/rc.local
+sed -i '$isleep 30\nip route add 225.0.0.0\/24 dev eth2\nmodprobe bcm2835-v4l2' /etc/rc.local
+
+# if the interface are swapped
+# sed -i -e 's/eth1/eth2/g' /etc/dhcpcd.conf
+# sed -i -e 's/eth1/eth2/g' /etc/network/interfaces
+# sed -i -e 's/eth1/eth2/g' /etc/default/isc-dhcp-server
+# sed -i -e 's/eth1/eth2/g' /etc/iptables/rules.v4
+# sed -i -e 's/eth2/eth1/g' /etc/rc.local
