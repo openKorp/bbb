c4d6f42a47367e30b5adec7b5a176b18f9c731f1
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-03-28 10:10:26 +0200

    Added eth0 support

--- rpi3/install.sh
+++ rpi3/install.sh
@@ -63,6 +63,9 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
 iptables -A FORWARD -i wlan0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth1 -o wlan0 -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -A FORWARD -i eth0 -o eth1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 iptables-save > /etc/iptables/rules.v4
 
 # mutlicast
